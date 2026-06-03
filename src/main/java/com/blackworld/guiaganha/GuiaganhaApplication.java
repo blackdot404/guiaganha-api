3da61189c6c1901c1a0ff714fe191eed76afbde5
blackdot404 <blackstv11@gmail.com>
2026-06-03 20:48:34 -0300

    feat: projeto springboot

---
+++ src/main/java/com/blackworld/guiaganha/GuiaganhaApplication.java
@@ -0,0 +1,13 @@
+package com.blackworld.guiaganha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiaganhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiaganhaApplication.class, args);
+	}
+
+}
